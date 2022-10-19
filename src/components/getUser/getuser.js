d1546c2a7a7e5b90121b0a006350b755a72d6718
fhelipe0203 <octaviouriel2002@gmail.com>
2022-10-18 23:24:54 -0300

    juntando 2

--- src/components/getUser/getuser.js
+++ src/components/getUser/getuser.js
@@ -1,6 +1,5 @@
 import React,{ useState,useEffect} from 'react';
 import axios from 'axios';
-
 import Table from 'react-bootstrap/Table';
 
 
@@ -28,7 +27,7 @@ function Getuser() {
           
 
            <Table striped bordered hover="sm">
-        <thead>
+        <thead className='table-container' >
           <tr>
             <th>id</th>
             <th>nome</th>
